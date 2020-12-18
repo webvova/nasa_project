62c65ea462530fef46e35b409de7235b94a09d36
webvova <webvova8@gmail.com>
2020-12-18 16:19:40 +0200

    Updates

---
+++ precache-manifest.03c068492546509676a518f61ad541e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35ae136021f76d3ad549b8eafece5bcf",
+    "url": "/webvova/nasa_project/index.html"
+  },
+  {
+    "revision": "fe1dfe0eff68ddb68e73",
+    "url": "/webvova/nasa_project/static/css/2.2d167569.chunk.css"
+  },
+  {
+    "revision": "f53f0b5e5f5c3fab6044",
+    "url": "/webvova/nasa_project/static/css/main.39224d7d.chunk.css"
+  },
+  {
+    "revision": "fe1dfe0eff68ddb68e73",
+    "url": "/webvova/nasa_project/static/js/2.6f7d1427.chunk.js"
+  },
+  {
+    "revision": "e3cddcc14d0b349b4e6eccba623e55ae",
+    "url": "/webvova/nasa_project/static/js/2.6f7d1427.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f53f0b5e5f5c3fab6044",
+    "url": "/webvova/nasa_project/static/js/main.ef107bf0.chunk.js"
+  },
+  {
+    "revision": "e080b56f6537f0010a9e",
+    "url": "/webvova/nasa_project/static/js/runtime-main.24bb6193.js"
+  }
+]);
